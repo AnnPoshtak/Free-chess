c5423e39d1c263616d30b6ca0c16c1152f39a237
AnnPoshtak <annposhtak@gmail.com>
2026-02-22 13:48:51 +0200

    feat(header): Add header with logo and settings button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import BoardLayout from "./components/BoardLayout/BoardLayout";
+import Header from "./components/Header/Header";
 import { Container } from "./components/Container/Container";
 
 const App: React.FC = () => {
   return (
     <Container>
+        <Header />
       <BoardLayout />
     </Container>
   );
