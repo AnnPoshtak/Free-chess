92b82bf6cd19e247773e007e3c4aa5d3f68ffaeb
AnnPoshtak <annposhtak@gmail.com>
2026-03-05 15:42:22 +0200

    feat(settings): Created Settings modal. But now it doesnt change anything

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,25 @@ import "./App.css";
 import BoardLayout from "./components/BoardLayout/BoardLayout";
 import Header from "./components/Header/Header";
 import { Container } from "./components/Container/Container";
+import React, { useState } from 'react';
+import Settings from './components/Settings/Setings.tsx';
+import type { ChessSettings } from './common/interface/ChessSettings.tsx';
 
 const App: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [appSettings, setAppSettings] = useState<ChessSettings | null>(null);
   return (
     <Container>
-        <Header />
-      <BoardLayout />
-    </Container>
+  <Header onOpenSettings={() => setIsModalOpen(true)} />
+  
+  <BoardLayout/>
+  
+  <Settings 
+    isOpen={isModalOpen} 
+    onClose={() => setIsModalOpen(false)}
+    onSettingsChange={(newSettings) => setAppSettings(newSettings)}
+  />
+</Container>
   );
 };
 
