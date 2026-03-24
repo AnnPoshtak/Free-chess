14f44405297675928dcdd08f8f1085ad0978c9ff
AnnPoshtak <annposhtak@gmail.com>
2026-03-24 14:38:06 +0200

    feat(backend): add feature to change fen data

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,6 @@ import { Server } from 'socket.io';
 import cors from 'cors';
 
 const app = express();
-// allow requests from frontend
 app.use(cors());
 
 const server = http.createServer(app);
@@ -13,12 +12,11 @@ let waitingForGame: string[] = [];
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", //frontend link
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"]
   }
 });
 
-// function to start a game if we have 2 players
 function startGame() {
   if (waitingForGame.length < 2) return;
 
@@ -26,26 +24,28 @@ function startGame() {
   const player2Id = waitingForGame.shift();
 
   if (player1Id && player2Id) {
-    // create unique room
     const room = "gameRoom_" + Date.now();
 
-    // find real players by their IDs and put them in the room
-  io.sockets.sockets.get(player1Id)?.join(room);
-  io.sockets.sockets.get(player2Id)?.join(room);
+    io.sockets.sockets.get(player1Id)?.join(room);
+    io.sockets.sockets.get(player2Id)?.join(room);
 
-    // tell players in this room that game started
-    io.to(room).emit("game_started", { roomId: room });
+    io.to(player1Id).emit("game_started", { roomId: room, color: "w" });
+    io.to(player2Id).emit("game_started", { roomId: room, color: "b" });
+    
     console.log(`Game started in room: ${room}`);
   }
 }
 
-// when new player opens the site
 io.on('connection', (socket) => {
   console.log('New Player joined! ID:', socket.id);
   waitingForGame.push(socket.id);
   startGame();
 
-  // when player leaves the site
+  socket.on("newFen", (data) => {
+    console.log(`Received move in room ${data.roomId}`);
+    socket.to(data.roomId).emit("board_updated", data.fen);
+  });
+
   socket.on('disconnect', () => {
     console.log('Player disconnected:', socket.id);
     waitingForGame = waitingForGame.filter(id => id !== socket.id);
@@ -54,5 +54,5 @@ io.on('connection', (socket) => {
 
 const PORT = 4000;
 server.listen(PORT, () => {
-  console.log(`Server works on port ${PORT} 🚀`);
+  console.log(`Server works on port ${PORT}`);
 });
